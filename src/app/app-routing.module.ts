0a9f5c7720f30caaf69cfaffbfd33098448667cf
Henrique Novaes <henriquefaria.3237@aluno.unibh.br>
2021-05-25 22:14:40 -0300

    primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,29 @@ const routes: Routes = [
   {
     path: '',
     redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+    loadChildren: () =>
+      import('./folder/folder.module').then((m) => m.FolderPageModule),
+  },
+  {
+    path: 'Amigos',
+    loadChildren: () =>
+      import('./Amigos/amigos.module').then((m) => m.AmigosPageModule),
+  },
+  {
+    path: 'Viagens',
+    loadChildren: () =>
+      import('./Viagens/viagens.module').then((m) => m.ViagensPageModule),
+  },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
